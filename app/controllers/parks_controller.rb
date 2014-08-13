06b448043dc5464fc3dcde48d422b5dddb453db4
Kevin Austin <kevinaustin@hotmail.com>
2014-08-12 18:21:09 -0700

    initial parks scaffold complete

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,31 @@
+class ParksController < ApplicationController
+	def index
+		@parks= Park.all
+	end
+
+	def new
+		
+	end
+	
+	def create
+		
+	end
+	
+	def show
+		
+	end
+	
+	def edit
+		
+	end
+	
+	def update
+		
+	end
+	
+	def destroy
+    end
+end
+
+#Your Ass has been CRUDed.
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140808233227) do
+ActiveRecord::Schema.define(version: 20140813000200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20140808233227) do
     t.string   "open_space"
     t.string   "parking_lot"
     t.string   "restrooms"
-    t.string   "comments"
+    t.text     "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
