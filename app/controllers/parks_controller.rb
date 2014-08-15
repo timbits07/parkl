2e258b272e25cc281c679d0cbb650e4cb67f976a
Kevin Austin <kevinaustin@hotmail.com>
2014-08-14 17:11:49 -0700

    park model nearly complete

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -4,28 +4,48 @@ class ParksController < ApplicationController
 	end
 
 	def new
-		
+		@park= Park.new
 	end
 	
 	def create
-		
+		@park= Park.new(park_params)
+		if @park.save
+			redirect_to parks_path	
+		else
+      		render :action => 'new'
+    	end
 	end
 	
 	def show
-		
+		@park= Park.find params[:id]
 	end
 	
 	def edit
-		
+		@park = Park.find params[:id]
 	end
 	
 	def update
-		
+		@park = Park.find params[:id]
+    	if @park.update_attributes(park_params)
+       	   redirect_to parks_path
+    	else
+      	render :action => :edit
+   		end
 	end
-	
 	def destroy
+		@park = Park.find params[:id]
+    	@park.destroy
+    	redirect_to parks_path
     end
+
+
+private
+
+	def park_params
+		params.require(:park).permit(:name, :location, :equipment, :hours, :open_space, :parking_lot, :restrooms, :comments)
+	end
+
 end
 
-#Your Ass has been CRUDed.
+
 
--- app/models/park.rb
+++ app/models/park.rb
@@ -1,2 +1,10 @@
 class Park < ActiveRecord::Base
+	validates :name, presence: true
+	validates :location, presence: true
+	validates :equipment, presence: true
+	validates :hours, presence: true
+	validates :open_space, presence: true
+	validates :parking_lot, presence: true
+	validates :restrooms, presence: true
+	validates :comments, presence: true
 end
